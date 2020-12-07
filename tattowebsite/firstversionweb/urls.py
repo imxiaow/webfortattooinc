8e2fac7797d61e5b3c2e9de42fbfd33dd90ce229
Xiao Wang <xiaow.wang@mail.utoronto.ca>
2020-12-07 11:30:31 -0500

    fav icon

--- tattowebsite/firstversionweb/urls.py
+++ tattowebsite/firstversionweb/urls.py
@@ -5,8 +5,13 @@ urlpatterns = [
     path('', views.homePage, name='home'),
 
     path('about/', views.aboutUs, name="about"), #introduction to Tattoo company
-    path('contact/', views.contactUs, name="contact"), 
+    path('gallery/', views.galleryPage, name="gallery"),
+    path('artists/', views.artistsPage, name="artists"), 
     path('tattodesign/', views.tattoDesign, name="tattodesign"), 
+    path('tattooaftercare/', views.tattooAfterCarePage, name="tattooaftercare"),
+    path('contact/', views.contactUs, name="contact"), 
+    
+
 
     #tattodesign
     #contact
--- tattowebsite/firstversionweb/views.py
+++ tattowebsite/firstversionweb/views.py
@@ -16,11 +16,35 @@ def homePage(request):
     return render(request, "firstversionweb/homepage.html", context)
     #return HttpResponse('home')
 
+
 def aboutUs(request):
     context = {}
 
-
     return render(request, "firstversionweb/aboutus.html", context)
+
+
+def galleryPage(request):
+    context = {}
+    
+    return render(request, "firstversionweb/gallerypage.html", context)
+
+
+def artistsPage(request):
+    context = {}
+
+    return render(request, "firstversionweb/artists.html", context)
+
+
+def tattoDesign(request):
+    context = {}
+
+    return render(request, "firstversionweb/tattodesign.html", context)
+
+
+def tattooAfterCarePage(request):
+    context = {}
+
+    return render(request, "firstversionweb/tattooaftercarepage.html", context)
 
 
 def contactUs(request):
@@ -28,7 +52,7 @@ def contactUs(request):
 
     return render(request, "firstversionweb/contactus.html", context)
 
-def tattoDesign(request):
-    context = {}
 
-    return render(request, "firstversionweb/tattodesign.html", context)
+
+
+
