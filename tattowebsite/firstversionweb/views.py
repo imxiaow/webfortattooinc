670e56356071ea0d9a4f271739b55bd9b4560cf7
Xiao Wang <xiaow.wang@mail.utoronto.ca>
2020-12-11 17:19:52 -0500

    change the form submittion to redirect instead of reder

--- tattowebsite/firstversionweb/views.py
+++ tattowebsite/firstversionweb/views.py
@@ -44,7 +44,7 @@ def homePage(request):
             )
         company_send_out_email.fail_silently=False
         company_send_out_email.send()
-
+        return redirect('home')
         return render(request, "firstversionweb/homepage.html", {})
 
     else:
