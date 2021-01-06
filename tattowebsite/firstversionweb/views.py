90dffd1e4c6d6dd9fe231ceb032e523825922ae1
Xiao Wang <xiaow.wang@mail.utoronto.ca>
2021-01-05 21:52:11 -0500

    done microsoft bing map contact page

--- tattowebsite/firstversionweb/views.py
+++ tattowebsite/firstversionweb/views.py
@@ -115,10 +115,64 @@ def tattooAfterCarePage(request):
 
 
 def contactUs(request):
-    context = {}
 
-    return render(request, "firstversionweb/contactus.html", context)
+    if request.method == 'POST':
+        # get the form information
+        firstname = request.POST.get('firstname') # grab the data
+        lastname = request.POST.get('lastname')
+        Artists = request.POST.get('Artists')
+        emailaddress = request.POST.get('emailaddress')
+        subjectmessage = request.POST.get('subjectmessage')
 
+        info_context = {'firstname':firstname, 'lastname':lastname, 'Artists':Artists, 'emailaddress':emailaddress, 'subjectmessage':subjectmessage}
+
+        ''' Begin reCAPTCHA validation '''
+        recaptcha_response = request.POST.get('g-recaptcha-response')
+        url = 'https://www.google.com/recaptcha/api/siteverify'
+        values = {
+            'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
+            'response': recaptcha_response
+        }
+        data = urllib.parse.urlencode(values).encode()
+        req =  urllib.request.Request(url, data=data)
+        response = urllib.request.urlopen(req)
+        result = json.loads(response.read().decode())
+        ''' End reCAPTCHA validation '''
+
+        if result['success']:
+            messages.success(request, 'Contact form submitted with success!')
+            
+            # send the form as email to company
+            send_mail(
+                'Contact Form Submission from ' + firstname + ' ' + lastname, #subject
+                'The Contact Form Submission is the following: \n' + "Customer Name: " + firstname + ' ' + lastname + "\n" + "Artists: " + Artists + "\n" + "Customer Email Address: " + emailaddress + "\n" + "Message: \n" + subjectmessage + "\n", # message
+                emailaddress, # senders email address
+                ['info@wildcranetattoos.com'],#List of To EMail Address
+                fail_silently=False
+                )
+            
+            # automatically send an email to the form sender to confirm their submission.
+            auto_send_mail_template = render_to_string("firstversionweb/automatic_email_companyToCustomer_temp.html", {'firstname':firstname})
+            company_send_out_email = EmailMessage(
+                'auto-reply: Contact Form Submission - ' + firstname, 
+                auto_send_mail_template, 
+                settings.EMAIL_HOST_USER,
+                [emailaddress]
+                )
+            company_send_out_email.fail_silently=False
+            company_send_out_email.send()
+
+        else:
+            messages.error(request, 'Error: Invalid reCAPTCHA. Please try again.')
+
+        return redirect(reverse('contact')+ "#contactform")
+
+    else:
+        context = {}
+        return render(request, "firstversionweb/contactus.html", context)
+    #return HttpResponse('home')
+
+    #status=206
 
 
 
